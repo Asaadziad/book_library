c8b20b4567dae8afd983738c54d458c94350c7b1
Asaadziad <asaadabuelhija@gmail.com>
2022-09-19 19:22:22 +0300

    added book cards

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 
+const closeBtn = document.getElementById("closeBtn");
+
+const addBtn = document.getElementById("add");
+
+const result = document.getElementById("result");
+
 function book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,19 +13,49 @@ function book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  let bTitle = document.getElementById("title").value;
-  let bAuthor = document.getElementById("author").value;
-  let bPages = document.getElementById("pages").value;
-  let bRead = document.getElementById("read").value;
-  myLibrary += [bTitle, bAuthor, bPages, bRead];
-}
-
 function addBook() {
   document.getElementById("form-library-container").style.display = "flex";
 }
-const closeBtn = document.getElementsByClassName("closeBtn")[0];
 
-closeBtn.onclick = function () {
+addBtn.addEventListener("click", addBookToLibrary);
+
+closeBtn.addEventListener("click", function () {
   document.getElementById("form-library-container").style.display = "none";
-};
+});
+
+function addBookToLibrary() {
+  event.preventDefault();
+  document.getElementById("form-library-container").style.display = "none";
+  const bTitle = document.getElementById("title").value;
+  const bAuthor = document.getElementById("author").value;
+  const bPages = document.getElementById("pages").value;
+  const bRead = document.getElementById("read").value;
+  const bAdd = new book(bTitle, bAuthor, bPages, bRead);
+  myLibrary.push(bAdd);
+  createBookDOM(bAdd);
+}
+
+function createBookDOM(book) {
+  const newBook = document.createElement("div");
+  const newBookTitle = document.createElement("div");
+  const newBookAuthor = document.createElement("div");
+  const newBookPages = document.createElement("div");
+  const newBookRead = document.createElement("div");
+  const trash = document.createElement("i");
+  const edit = document.createElement("i");
+  trash.setAttribute("class", "fa fa-trash");
+  trash.setAttribute("id", "trashCan");
+  edit.setAttribute("class", "fa fa-pencil-square-o");
+  edit.setAttribute("id", "pencil");
+  newBook.setAttribute("id", "bookCard");
+  newBookTitle.setAttribute("id", "header");
+  newBookRead.setAttribute("id", "footer");
+  newBookTitle.textContent += book.title;
+  newBookTitle.appendChild(trash);
+  newBookTitle.appendChild(edit);
+  newBookRead.textContent += book.Read;
+  newBook.appendChild(newBookTitle);
+  newBook.appendChild(newBookRead);
+
+  result.appendChild(newBook);
+}
